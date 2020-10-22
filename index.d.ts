28e5922bea80e5e3f92ca288816650cb85f1b961
VinhNT <vinhnt@edoctor.vn>
2020-10-22 15:44:36 +0700

    add d.ts file

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export function showShortTop(message: string): void;
+export function showShortCenter(message: string): void;
+export function showShortBottom(message: string): void;
+export function showLongTop(message: string): void;
+export function showLongCenter(message: string): void;
+export function showLongBottom(message: string): void;
+export function show(message: string): void;
+export function hide(): void;
